08e7b91b89d72f739700592df8894b50e56ea915
JT <jazminteng@gmail.com>
2020-07-30 21:09:33 -0300

    back modelo 5 arreglado

--- src/components/ModeloStock/Functions/ModeloSimpleSinAgotamientoPorLote.js
+++ src/components/ModeloStock/Functions/ModeloSimpleSinAgotamientoPorLote.js
@@ -1,4 +1,5 @@
 const {CalcularN,CalcularToComun,CostoTotalPreparacionComun,CostoTotalProductoComun,CalcularqoComun } = require("./FuncionesComunes");
+const { mode } = require("mathjs");
 
 function CostoTotalAlmacenamiento(q,T,C1){
     let ctalm = 0.5 * T * q * C1;
@@ -28,6 +29,7 @@ function ModeloSimpleSinAgotamientoPorLote(D,q,K,PorcCostoProd,T,P,bi,Cientrada)
     let x = 0;
     let limsup = Number.MAX_VALUE;
     let flag = true;
+    let xoptimo;
     while (flag && x < bi.length){
         cdi = CostoDineroInmovilizado(P,bi[x]);
         Ci = CalcularCi(bi[x],PorcCostoProd)+Cientrada;
@@ -39,14 +41,63 @@ function ModeloSimpleSinAgotamientoPorLote(D,q,K,PorcCostoProd,T,P,bi,Cientrada)
         limsup = q[x];
         x++;
     }
-    const liminf = q[x-1];
-    const cprod = bi[x-1];
+    let ctprep, ctprod, ctalm, cte;
+    xoptimo = x-1;
+    let array = new Array();
+    let cprod = bi[xoptimo];
+    ctprep = CostoTotalPreparacionComun(D,qo,K);
+    ctprod = CostoTotalProductoComun(cprod, D);
+    ctalm = CostoTotalAlmacenamiento(qo,T,C1);
+    cte = ctprep + ctprod + ctalm;
+    let model = new Object();
+    model.cte = cte;
+    model.qo = qo;
+    model.bi = cprod;
+    model.liminf = q[xoptimo];
+    model.ctprep = ctprep;
+    model.ctprod = ctprod;
+    model.ctalm = ctalm;
+    array.push(model);
+    let modelmin = new Object();
+    modelmin = model;
+    if (xoptimo !== 0) {
+        for (x=xoptimo-1; x>=0; x--){
+            liminf = q[x];
+            qo = liminf;
+            cdi = CostoDineroInmovilizado(P,bi[x]);
+            Ci = CalcularCi(bi[x],PorcCostoProd)+Cientrada;
+            C1 = CalcularC1(cdi,Ci);
+            cprod = bi[x];
+            ctprep = CostoTotalPreparacionComun(D,qo,K);
+            ctprod = CostoTotalProductoComun(cprod, D);
+            ctalm = CostoTotalAlmacenamiento(qo,T,C1);
+            cte = ctprep + ctprod + ctalm;
+            model.cte = cte;
+            model.qo = qo;
+            model.bi = bi[x];
+            model.liminf = liminf;
+            model.ctprep = ctprep;
+            model.ctprod = ctprod;
+            model.ctalm = ctalm;
+            array.push(model);
+        }
+        ctemin =Number.MAX_VALUE;
+        for (i=0; i<array.length; i++) {
+            if (array[i].cte < ctemin) {
+                modelmin = array[i];
+                ctemin = array[i].cte;
+            }
+        }
+    }
     const n = CalcularN(qo,D);
     const To = CalcularToComun(K,T,D,C1);
-    const ctprep = CostoTotalPreparacionComun(D,qo,K);
-    const ctprod = CostoTotalProductoComun(cprod, D);
-    const ctalm = CostoTotalAlmacenamiento(qo,T,C1);
-    const cte = ctprep + ctprod + ctalm;
+    ctprep = modelmin.ctprep;
+    ctalm = modelmin.ctalm;
+    ctprod = modelmin.ctprod;
+    cte = modelmin.cte;
+    qo = modelmin.qo;
+    liminf = modelmin.liminf;
+    cprod = modelmin.bi;
     return {n,To,ctprep,ctprod,ctalm,cte,qo,liminf,cprod}
     //q[x-1] = limite inferior que se tomo el qo
     //bi[x-1] = el precio que tomo el qo
