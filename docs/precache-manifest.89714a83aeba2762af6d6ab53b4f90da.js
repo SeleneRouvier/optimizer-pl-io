0a6ed50d910c305cf7d2850692e2b973ea375653
Selene Rouvier <selenerouvier25@outlook.com>
2020-09-05 21:06:30 -0300

    docs agregado

---
+++ docs/precache-manifest.89714a83aeba2762af6d6ab53b4f90da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7bf6a7068ee75d8b419cd88560eef2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "040af190d4f9bb211639",
+    "url": "./static/css/2.2f8ebd95.chunk.css"
+  },
+  {
+    "revision": "dd207c3f10e1a4b8edee",
+    "url": "./static/css/main.f4b785f1.chunk.css"
+  },
+  {
+    "revision": "040af190d4f9bb211639",
+    "url": "./static/js/2.cf6f935f.chunk.js"
+  },
+  {
+    "revision": "8a5ac7ab10132125faaf70b1e3b19f2d",
+    "url": "./static/js/2.cf6f935f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd207c3f10e1a4b8edee",
+    "url": "./static/js/main.f60d557e.chunk.js"
+  },
+  {
+    "revision": "0d87a42795fa5bc9ab28",
+    "url": "./static/js/runtime-main.f843f7a9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
